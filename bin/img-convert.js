ae22b587a93a0dc1c9bbe8d0d82b6d77dc35c171
songjun <songjun@broada.com>
2019-09-27 10:26:47 +0800

    update： 图片文件转base64 编码文件

--- bin/img-convert.js
+++ bin/img-convert.js
@@ -19,27 +19,31 @@ if (argv.help || argv.h) {
 }
 
 let options = {
-  file: argv._[0],
-  path: argv._[1]
+  filePath: argv._[0],
+  outPath: argv._[1]
 }
 
-if (argv.n || argv.name) {
-  // 文件名称合法校验
+if (argv.n || argv.name) {   // 文件名称合法校验
+
   options.outPutName = argv.n || argv.name
 }
 
-if (argv.e || argv.ext) {
-  // 文件扩展名校验
-  options.outPutName = argv.n || argv.name
+if (argv.e || argv.ext) {   // 文件扩展名校验
+
+  options.extname = argv.e || argv.ext
 }
 
 
-if (argv.i || argv.img) {
+
+if (argv.i || argv.img) {    // base64转换成图片
   options.convertType = 'img'
+ 
+
+  
+} else if (argv.b || argv.base64) {  //图片转换成base64
+  options.convertType = 'base64'
   ImgConvert.imgConvertToBase64(options)
 
-} else if (argv.b || argv.base64) {
-  options.convertType = 'base64'
 
 } else {
   console.log(colors.red('Please input convert method!'))
